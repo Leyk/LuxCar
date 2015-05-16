14c61b3e2c1863700b60688232444f22bafc6463
Leyk <savornin@msn.com>
2015-05-16 21:58:53 +0200

    vues connexion et menu

---
+++ vues/v_menu.php
@@ -0,0 +1,27 @@
+<div class="row">
+	<nav class="col-md-12">
+		<ul class="list-inline">
+			<li>
+				<a href="index.php?uc=accueil">Accueil</a>
+			</li>
+			<li>
+				<a href="index.php?uc=catalogue&action=consulterCatalogue">Catalogue</a>
+			</li>
+			<li>
+				<a href="index.php?uc=catalogue&action=demandeDevis">Créer un devis</a>
+			</li>
+			<li>
+				<a href="index.php?uc=administration&action=demanderAdministration">Administration</a>
+			</li>
+			<li>
+				<a href="index.php?uc=inscription&action=demandeInscription">Inscription</a>
+			</li>
+			<li>
+				<a href="index.php?uc=connexion&action=demandeConnexion">Connexion</a>
+			</li>
+			<li>
+				<a href="index.php?uc=connexion&action=deconnexion">Deconnexion</a>
+			</li>
+		</ul>
+	</nav>
+</div>
